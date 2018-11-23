3499ba98e9268a5c04911f18801cf4f725cfec50
Barbariansyah <43785141+Barbariansyah@users.noreply.github.com>
2018-11-23 14:52:47 +0700

    Add files via upload

---
+++ load.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mesinkata.c"
+
+void load()
+{
+
+
+    Kata data[20][50];
+
+    int p,q;
+    int a,b,c,d;
+    int j =1;
+    p = 1; q = 1;
+    STARTKATA();
+    while (!EndKata) {
+        data[p][q] = CKata;
+        ADVKATA();
+
+        if(CKata.TabKata[1] == '|')
+        {
+            p++;
+            q = 0;
+            ADVKATA();
+        }
+        q++;
+        }
+
+//Ngeliat bentuk datanya kaya gimana
+ /*    for (a=1; a<=3; a++)
+     {
+         for (b=1;b<=5; b++)
+         {
+             for (c=1; c<=data[a][b].Length;c++)
+             {
+                 printf("%c",data[a][b].TabKata[c]);
+             }
+             printf("|");
+         }
+         printf("\n");
+     }
+/*         for(k=1; k<=4; k++)
+    {
+            for (i=1;i<=data[k][1].Length;i++) {
+                printf("%c", data[k][1].TabKata[i]);
+                }
+            printf("|");
+    }*/
+}
+
+
+int main()
+{
+
+pita = fopen("player_one.txt","r");
+load();
+
+
+
+
+/*    int i,k;
+    int j =1;
+    STARTKATA();
+    while (!EndKata) {
+        m[j] = CKata;
+        /*for (i=1;i<=CKata.Length;i++) {
+        printf("%c",CKata.TabKata[i]);
+        }
+        printf("\n");
+        ADVKATA();
+        j++;
+        }
+    for(k=1; k<=4; k++)
+    {
+            for (i=1;i<=m[k].Length;i++) {
+                printf("%c", m[k].TabKata[i]);
+                }
+            printf("|");
+    }
+*/
+ return 0;
+}
