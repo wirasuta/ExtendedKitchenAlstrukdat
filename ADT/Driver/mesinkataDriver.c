7fcc691108ed0d5491930bb99cf6988480cd1b3a
kintansekar <43615478+kintansekar@users.noreply.github.com>
2018-11-25 10:55:17 +0700

    Add files via upload

---
+++ ADT/Driver/mesinkataDriver.c
@@ -0,0 +1,34 @@
+#include "mesinkata.c"
+
+int main (){
+    Kata K1,K2;
+    boolean found;
+
+    K1.TabKata[0]=' ';
+    K1.TabKata[1]='a';
+    K1.TabKata[2]='y';
+    K1.TabKata[3]='a';
+    K1.TabKata[4]='m';
+    K1.TabKata[5]='\0';
+    K1.Length=5;
+
+
+    K2.TabKata[0]=' ';
+    K2.TabKata[1]='a';
+    K2.TabKata[2]='y';
+    K2.TabKata[3]='a';
+    K2.TabKata[4]='m';
+    K2.TabKata[5]='\0';
+    K2.Length=5;
+
+    STARTKATA();
+    int i =0;
+
+    printf("length: %d", CKata.Length);
+    while(i < CKata.Length+1){
+        printf("cek isi CKata : %c\n",CKata.TabKata[i]);
+        i++;
+    }
+    printf("IsKataSama( K1, K2)= %d\n",IsKataSama( K1,  K2));
+
+}
